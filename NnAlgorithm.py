dfc3655d184aac99d96a19c46763a26b17251091
dandrue <d.ruedaq@gmail.com>
2022-02-03 12:19:36 -0500

    Cleaning NnAlgorithm.py

--- NnAlgorithm.py
+++ NnAlgorithm.py
@@ -8,13 +8,6 @@ This file contains the core of the Neural Network Algorithm. Its constructed fol
 """
 
 
-# TODO Add a cross validation feature
-# TODO Add different activation functions
-# TODO Add different cost functions,
-#  now the algorithm implement the squared difference, in the future it will implement the cost for logistic
-#  regression adding regularization, etc
-
-
 def sigmoid(z):
     """
     Activation function, it would be ReLu or any other activation function
@@ -32,9 +25,8 @@ class Network(object):
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.weights = [np.random.randn(y , x + 1) for x, y in zip(sizes[:-1], sizes[1:])]
-        # self.weights.append(np.random.randn(sizes[-1], sizes[-2]+1))
-        print(self.weights[0].shape, self.weights[1].shape, self.weights[2].shape)
+        self.weights = [np.random.randn(y + 1, x + 1) for x, y in zip(sizes[:-2], sizes[1:])]
+        self.weights.append(np.random.randn(sizes[-1], sizes[-2]+1))
 
     def feedforward(self, a):
         for w in self.weights:
@@ -45,7 +37,6 @@ class Network(object):
         training_data = list(train_data)
         n = len(training_data)
         n_test = 0
-        # print(training_data[0][0], training_data[0][0].shape)
 
         if test_data:
             test_data = list(test_data)
@@ -73,36 +64,21 @@ class Network(object):
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         activation = x
         activations = [x]
-
         zs = []
 
         for w in self.weights:
-            if len(activations) != 1:
-                activation = np.insert(activation,0,1.0, axis=0)
-            print(w.shape, activation.shape)
             z = np.dot(w, activation)
-            print(',')
-            print(z.shape)
             zs.append(z)
             activation = sigmoid(z)
-            # print(activation)
             activations.append(activation)
-        for z in zs:
-            print(z.shape)
-        # print(activations[-1])
+
         delta = self.cost_derivative(activations[-1], y)
-        print(delta.shape)
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
 
         for i in range(2, self.num_layers):
-            print(delta.shape)
-            # print(i)
             z = zs[-i]
             sp = sigmoid_prime(z)
-            print(self.weights[-i+1].shape)
-            print(sp.shape)
-            delta = np.dot(self.weights[-i+1].transpose()[1:], delta) * sp
-            # print(delta.shape)
+            delta = np.dot(self.weights[-i+1].transpose(), delta) * sp
             nabla_w[-i] = np.dot(delta, activations[-i-1].transpose())
         return nabla_w
 
